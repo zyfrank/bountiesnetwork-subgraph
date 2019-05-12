947e5454faca7931260b43a91818f9b3eb7abd7f
zyfrank <zy_frank@hotmail.com>
2019-05-12 10:21:23 +0800

    init implementation

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Example Subgraph
+# Bounties Network Subgraph
 
-An example to help you get started with The Graph. For more information see the docs on https://thegraph.com/docs/.
+A Subgraph used to explore Bounties Network smart contract
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,66 @@
-import { NewGravatar, UpdatedGravatar } from './types/Gravity/Gravity'
-import { Gravatar } from './types/schema'
+import { BountyIssued, BountyActivated, BountyFulfilled, FulfillmentUpdated, FulfillmentAccepted, BountyKilled, ContributionAdded, DeadlineExtended, BountyChanged, IssuerTransferred, PayoutIncreased, IssueBountyCall} from './types/BountiesNetwork/BountiesNetwork'
+import { Bounty } from './types/schema'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+
+export function handleBountyIssued(event: BountyIssued): void {
+  let bounty = new Bounty(event.params.bountyId.toString())
+  bounty.save()
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
+export function handleBountyActivated(event: BountyActivated): void {
+  let id = event.params.bountyId.toString()
+  let bounty = Bounty.load(id)
+  bounty.issuer = event.params.issuer
+  bounty.save()
+}
+
+export function handleBountyFulfilled(event: BountyFulfilled): void {
+
+}
+
+export function handleFulfillmentUpdated(event: FulfillmentUpdated): void {
+
+}
+
+export function handleFulfillmentAccepted(event: FulfillmentAccepted): void {
+
+}
+
+export function handleBountyKilled(event: BountyKilled): void {
+
+}
+
+export function handleContributionAdded(event: ContributionAdded): void {
+
+}
+
+export function handleDeadlineExtended(event: DeadlineExtended): void {
+
+}
+
+export function handleBountyChanged(event: BountyChanged): void {
+
+}
+
+export function handleIssuerTransferred(event: IssuerTransferred): void {
+
+}
+
+export function handlePayoutIncreased(event: PayoutIncreased): void {
+
+}
+
+export function handleIssueBounty(call: IssueBountyCall): void {
+  let id = call.outputs.value0.toString();
+  let bounty = Bounty.load(id)
+  if (bounty != null){
+  bounty.issuer = call.inputs._issuer
+  bounty.deadline = call.inputs._deadline
+  bounty.data = call.inputs._data
+  bounty.fulfillmentAmount = call.inputs._fulfillmentAmount
+  bounty.arbiter = call.inputs._arbiter
+  bounty.paysTokens = call.inputs._paysTokens
+  bounty.save()
   }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
 }
+
